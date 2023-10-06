de8841cf57f4b6d0bfd1fac8d77f9352ceef41b3
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:08:35 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (j för ja och n för nej)");
+            string gymnasie = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (gymnasie == "j" && ålder < 22)
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            else
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+
+            Console.ReadKey();
+        }
+    }
+}
